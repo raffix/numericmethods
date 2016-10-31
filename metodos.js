019a1f7361de063453be89a79d9f628a5ba0e1c5
sheepma <mateusaceto@hotmail.com>
2016-10-31 13:22:38 -0200

    Jacobi
    Falta melhorar o visual

--- metodos.js
+++ metodos.js
@@ -175,4 +175,115 @@ function newton()
 		$('#newtonIteracoes').html(String(i));
 		$('#newtonX').html(String(x));
 			
-}	
+}
+
+function matrixParser(x){
+	var order = x.indexOf(";");
+	order = (order+1)/2; 
+	x = x.replace(/;/g," ");
+	var y = x.split(" "); 
+	var matrix = [], i, k;
+    for (i = 0, k = -1; i < y.length; i++) {
+        if (i % order === 0) {
+            k++;
+            matrix[k] = [];
+        }
+        matrix[k].push(y[i]);
+    }
+	return matrix;
+	
+}
+
+function Jacobi() {
+	$('#JacobiIteracoes').html(" ");
+	$('#JacobiX').html(" ");
+	var matriz = $('#JacobiA').val();
+	var bb = $('#JacobiB').val();
+	var A = matrixParser(matriz);
+	var XX = $('#JacobiX0').val();
+	var X = XX.split(" ");
+	var x = new Array();
+	var E = parseFloat($('#JacobiEpson').val());
+	var b = bb.split(" ");
+	if (XX.length == 0) {	
+		for (var k = 0; k < b.length; k++)
+		{
+			X[k] = Math.floor((Math.random() * 10000) + 1);
+		}
+	}	
+	var m = 1000;//Numero máximo de iterações
+	var ni = 0;//Contador de iterações
+	var continuar = true;
+	var inter;
+
+	while (continuar && ni < m) {
+	    for (var i=0; i < b.length; i++) {
+	        soma = 0;
+	        for (var j = 0; j < b.length; j++) {
+	            if (j != i) {
+	                soma = soma + A[i][j]*X[j]/A[i][i];
+	            }
+	            x[i] = (b[i]/A[i][i]) - soma;
+	        }
+		}
+		inter = Math.abs(math.norm(x) - math.norm(X));
+	    if (inter < E) {
+	        continuar = false;
+		} else {
+	        X=x.slice(0);
+		}
+	    ni = ni + 1;
+	}
+	$('#JacobiIteracoes').html(String(ni));
+	for (i=0;i<X.length;i++)
+	{
+	$('#JacobiX').append("X["+i+"] = " + X[i] + "<br>");
+	}
+	
+}
+
+
+function gaussJacobi() {
+	var matriz = $('#GaussA').val();
+	var bb = $('#GaussB').val();
+	var A = matrixParser(matriz);
+	var XX = $('#GaussX0').val();
+	var X = XX.split(" ");
+	var x = new Array();
+	var E = parseFloat($('#GaussEpson').val());
+	var b = bb.split(" ");
+	if (XX.length == 0) {	
+		for (var k = 0; k < b.length; k++)
+		{
+			X[k] = Math.floor((Math.random() * 10000) + 1);
+		}
+	}	
+	var m = 1000;//Numero máximo de iterações
+	var ni = 0;//Contador de iterações
+	var continuar = true;
+	var inter;
+
+	while (continuar && ni < m) {
+	    for (var i=0; i < b.length; i++) {
+	        soma = 0;
+	        for (var j = 0; j < b.length; j++) {
+	            if (j != i) {
+	                soma = soma + A[i][j]*X[j]/A[i][i];
+	            }
+	            x[i] = (b[i]/A[i][i]) - soma;
+	        }
+		}
+		inter = Math.abs(math.norm(x) - math.norm(X));
+	    if (inter < E) {
+	        continuar = false;
+		} else {
+	        X=x.slice(0);
+		}
+	    ni = ni + 1;
+	}
+	$('#GaussIteracoes').html(String(ni));
+	$('#GaussX').html(String(X));
+}
+
+
+	
