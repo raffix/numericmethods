9e39cbb2325602590821b0797362865e7fede725
sheepma <mateusaceto@hotmail.com>
2017-03-02 10:01:29 -0300

    ajuste polinomial arrumado

--- js/ajustecurvas.js
+++ js/ajustecurvas.js
@@ -2,35 +2,115 @@
 	{		
 		$('#MQA').html(" ");
 		$('#MQB').html(" ");
+		$('plotMQ').html(" ");
 		if($('#MQX').val() == undefined || $('#MQY').val() == undefined){
 			return;
 		}		
 		var X =  $('#MQX').val();
-		var Y =  $('#MQY').val();		
+		var Y =  $('#MQY').val();
+		var order =  $('#MQK').val();
+		order = parseInt(order);	
 		var x = X.split(" ");
-		var y = Y.split(" ");
-		var n = x.length;
-		var i, sumX = 0, sumY = 0, sumX2 = 0, sumXY = 0, a, b, formula;	
-		var annotat = ' ';	
-		for(i = 0; i<n; i++){
-			sumX += x[i]*1;
-			sumY += y[i]*1;
-			sumX2 += Math.pow(x[i], 2);
-			sumXY += x[i]*y[i];
+		var y = Y.split(" ");		
+		var sy =[], sx=[], sxy=[];
+		var i,j,o = 1;
+		var matrixA = [], matrixB = [];
+		if(order>x.length){
+			alert("Grau do polinomio não deve execeder a quantidade de pontos");
+			return;
+		}
+		for(i = 0; i<order+order+1; i++){
+			sy[i] = 0;
+			sx[i] = 0;
+			sxy[i] = 0;	
+		}
+		for(i = 0; i<x.length; i++){
+			for(j = 1; j<order+order+1; j++){
+				sy[j] += math.pow(y[i], j);
+				sx[j] += math.pow(x[i], j);
+				sxy[j] += math.pow(x[i], j)*y[i];
+			}
 		}		
-		a = (sumXY * n - sumY * sumX)/(sumX2 * n - sumX * sumX)
-		b =(sumY * sumX2 - sumXY * sumX)/(n * sumX2 - sumX * sumX);
-		formula = a+'x +'+b
-		var data = '[{"fn": "'+formula+'"},{ "points": [['+x[0]+', '+y[0]+']';
-		for(i=1; i<n; i++){
-			data += ',['+x[i]+', '+y[i]+']'; 
+		for (i = 0; i < order+1; i++) {
+			matrixA[i] = [];						
+		}			
+		for(i = 0; i<order+1; i++){
+			for(j = 0; j<order+1; j++){
+				if(i == 0 && j == 0){
+					matrixA[i][j] = x.length;
+				}
+				else{
+					if(j == 0){
+						o = o-order;
+					}
+					matrixA[i][j] = sx[o];
+					o++;	
+				}
+			}	
+		}
+		matrixB[0] = sy[1];
+		for(i = 1; i<order+1; i++){
+			matrixB[i] = sxy[i];
+		}		
+		
+		var solution = math.lusolve(matrixA, matrixB);
+		solution = solution.reverse();
+		var r2y = y;
+		var r2x = x;		
+		var r2fun = solution;
+		var ym=0;
+		var r2s =0;
+		var	r2d =0;
+		for(i = 0; i<r2x.length; i++){
+			ym += r2y[i]/r2x.length;
+		}
+		var k = r2fun.length - 1;
+		var funcao = "";
+		var funcao2 = "";
+		for(i = 0; i<r2fun.length; i++){
+			if(k == 0){				
+				funcao += r2fun[i];
+				funcao2 += r2fun[i];	
+			}
+			else if(k == 1){
+				if(r2fun[i+1] < 0) {
+					funcao2 += r2fun[i] + 'x ';
+					funcao += r2fun[i] + '*x';
+				}
+				else {
+					funcao2 += r2fun[i] + 'x + ';
+					funcao += r2fun[i] + '*x+';
+				}
+				
+			}				
+			else{
+				if(r2fun[i+1] < 0) {
+					funcao2 += r2fun[i] + 'x^'+k + " ";
+					funcao += r2fun[i] + '*math.pow(x, '+k+')+';
+				}
+				else {
+					funcao2 += r2fun[i] + 'x^'+k + " + ";
+					funcao += r2fun[i] + '*math.pow(x, '+k+')+';
+				}				
+			}
+			k--;
+		}
+		var x;
+		for(i = 0; i<r2x.length; i++){
+			x = r2x[i];
+			r2s += math.pow(eval(funcao) - r2y[i], 2);
+			r2d += math.pow(r2y[i]-ym, 2);
+		}
+		var r2 = 1-(r2s/r2d);
+		var data = '[{"fn": "'+funcao2+'"},{ "points": [['+r2x[0]+', '+r2y[0]+']';
+		for(i=1; i<x.length; i++){
+			data += ',['+r2x[i]+', '+r2y[i]+']'; 
 		} 
 		data += '],"fnType": "points", "graphType": "scatter"}]'
 		var annotat = '{ }';
-		$('#MQA').append(a + "<br>");
-		$('#MQB').append(b);
-		console.log(data);
-		console.log(annotat);
-		drawChart(data, annotat, '#plotMQ');	
-		
+			
+		$('#MQA').append(funcao2 + "<br>");
+		$('#MQB').append(r2);
+		console.log(data);	
+		drawChart(data, annotat, '#plotMQ');			
 	}
